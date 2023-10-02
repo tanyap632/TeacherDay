6f5f3a0be4289933e3f407339d57473c3d70f95b
Tanya Poh <tanyap632@gmail.com>
2023-10-02 13:19:36 +0500

    CcC

--- script.js
+++ script.js
@@ -15,17 +15,3 @@ $("a:not([href*=\\#])").click(function () {
 });
 
 
-$(function () {
-  $(window).bind('resize', function () {
-    resizeMe();
-  }).trigger('resize');
-});
-//Standard height, for which the body font size is correct
-var preferredHeight = 768;
-//Base font size for the page
-var fontsize = 20;
-
-var displayHeight = $(window).height();
-var percentage = displayHeight / preferredHeight;
-var newFontSize = Math.floor(fontsize * percentage) - 1;
-$("body").css("font-size", newFontSize);
