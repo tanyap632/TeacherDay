073521c1c666202a1998188b5f1a0e035537a6bb
Tanya Poh <tanyap632@gmail.com>
2023-10-02 13:18:32 +0500

    hlghdgrkhjkfgk

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ $("a:not([href*=\\#])").click(function () {
     return false;
   }
 });
+
+
+$(function () {
+  $(window).bind('resize', function () {
+    resizeMe();
+  }).trigger('resize');
+});
+//Standard height, for which the body font size is correct
+var preferredHeight = 768;
+//Base font size for the page
+var fontsize = 20;
+
+var displayHeight = $(window).height();
+var percentage = displayHeight / preferredHeight;
+var newFontSize = Math.floor(fontsize * percentage) - 1;
+$("body").css("font-size", newFontSize);
